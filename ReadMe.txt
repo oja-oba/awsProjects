60e2f9e65cc001d319dfce60d812af1f593918c5
oja-oba <akinwaleoreoluwa05@gmail.com>
2023-05-25 19:03:54 +0100

    'object-detention'

---
+++ ReadMe.txt
@@ -0,0 +1,33 @@
+AWS Rekognition Object Detection Script
+
+This Python script allows you to connect to AWS Rekognition using the Boto3 client and perform object detection on images.
+Prerequisites
+
+Before running the script, make sure you have the following:
+
+    Python 3 installed
+    Boto3 library installed (pip install boto3)
+    AWS credentials configured (either through environment variables or AWS CLI)
+
+Installation
+
+    Clone this repository or download the script file.
+    Install the required dependencies by running pip install boto3.
+    Configure your AWS credentials using one of the methods mentioned in the prerequisites.
+
+Usage
+
+    Replace the placeholder values in the script with your own AWS access key, secret key, and region.
+    Provide the path to the image file you want to analyze by replacing the variable "photo" in the script.
+    Run the script using the command python object-detection.py.
+    The script will connect to AWS Rekognition and perform object detection on the provided image.
+    The detected objects along with their confidence scores will be displayed on the image  as the output.
+
+Notes
+
+    Ensure that the image you provide is in a supported format (JPEG, PNG, etc.) and is accessible to the script.
+    Make sure your AWS credentials have the necessary permissions to access AWS Rekognition and perform object detection.
+
+Feel free to customize and enhance this script based on your specific requirements and use cases.
+
+If you have any questions or issues, please feel free to reach out. Happy object detection with AWS Rekognition!
---
+++ object-detention.py
@@ -0,0 +1,63 @@
+import boto3
+import csv
+from PIL import Image, ImageDraw, ImageFont
+import io
+
+#read the secret.csv file that contains the key id and access key 
+with open('secret.csv', 'r') as file :
+    next(file)
+    reader =csv.reader(file)
+    
+    
+    for line in reader:
+        access_key_id  =line[0]
+        secret_access_key =line[1]
+        
+  # connect to the aws rekognitoon with the credentials and assign to variable client    
+client = boto3.client('rekognition',region_name = 'us-west-2', aws_access_key_id = access_key_id, aws_secret_access_key = secret_access_key)
+
+#replace with the desire path to the image you want to run the script on 
+photo ='flower.jpg'
+
+with open(photo,'rb') as image_file :
+    source_bytes = image_file.read()
+    
+    
+detect_objects = client.detect_labels(Image={'Bytes' : source_bytes})
+
+image = Image.open(io.BytesIO(source_bytes))
+draw = ImageDraw.Draw(image)
+ 
+for label in detect_objects['Labels']:
+    print(label['Name'] )
+    print('Confidence', label['Confidence'])
+    
+    
+    for instances in label['Instances']:
+        if 'BoundingBox' in instances:
+            
+            box= instances['BoundingBox']
+            
+            left = image.width * box['Left']
+            top = image.height * box['Top']
+            width = image.width * box['Width']
+            height = image.width * box['Height']
+            
+            points=(
+                (left,top),
+                (left+width, top ),
+                (left+width, top+height),
+                (left, top +height),
+                (left,top)            
+
+                
+                )
+            draw.line(points, width=5, fill ='#69f5d9')
+            
+            shape = [(left - 2, top - 35), (width + 2 + left, top )]
+            draw.rectangle(shape, fill='#69f5d9')
+            font = ImageFont.truetype('sketch.ttf', 30)
+            name = label['Name']+ ' ' + str(int(label['Confidence']))
+            draw.text((left + 10, top - 30), name, font=font, fill='#000000')
+            
+image.show()
